bf1dee956a799994673541eb45d134c3280ec992
Severin Brunner <severin.brunner@t-online.de>
2017-10-11 03:17:03 +0200

    finished first version of Frequency Display Canvas

--- src/GUI/AudioFrequencyDisplay.java
+++ src/GUI/AudioFrequencyDisplay.java
@@ -2,6 +2,7 @@ package GUI;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.sound.sampled.AudioFormat;
@@ -10,7 +11,7 @@ import javax.sound.sampled.AudioFormat;
 @SuppressWarnings("serial")
 public class AudioFrequencyDisplay extends Canvas {
 	
-	private boolean stop;
+	private boolean isAnalysing;
 	
 	private byte[] bSignal = null;
 	private short[] sSignal = null;
@@ -18,6 +19,18 @@ public class AudioFrequencyDisplay extends Canvas {
 	
 	private int minFreq = 0;
 	private int maxFreq = 20000;
+	private int maxAmp = 100;
+	
+	//precision value is the 'range' of one frequency; e.g. precision of 2 means
+	//that a frequency of 400 is for all frequencies of the range between 399 and 401 Hz.
+	private int precision;
+	
+	private Color bgColor = new Color(240, 240, 240);
+	private Color scaleColor = new Color(10, 10, 10);
+	private Color graphColor = new Color(20, 255, 20);
+	
+	private int scaleTextOffsetX = 20;
+	private final int scaleTextOffsetY = 15;
 	
 	public AudioFrequencyDisplay(int minFrequency, int maxFrequency, int length, int width) {
 		super();
@@ -25,7 +38,7 @@ public class AudioFrequencyDisplay extends Canvas {
 		this.maxFreq = maxFrequency;
 		this.setSize(length, width);
 		
-		this.setBackground(new Color(220, 220, 220));
+		this.setBackground(bgColor);
 		this.setVisible(true);
 	}
 	
@@ -35,8 +48,8 @@ public class AudioFrequencyDisplay extends Canvas {
 			throw new IllegalArgumentException("A 8-bit signal was given, however the AudioFormat isn't 8 bits");
 		}
 		bSignal = signal;
-		this.format = format;
-		
+		this.setFormat(format);
+		this.precision = (int) (format.getSampleRate()) / signal.length;
 		this.startAnalysis();
 	}
 	
@@ -46,45 +59,107 @@ public class AudioFrequencyDisplay extends Canvas {
 			throw new IllegalArgumentException("A 16-bit signal was given, however the AudioFormat isn't 16 bits");
 		}
 		sSignal = signal;
-		this.format = format;
+		this.setFormat(format);
 		
 		this.startAnalysis();
 	}
 	
-	private void drawScale() {
-		
+	public void updateGraph() {
+		repaint();
 	}
 	
 	public void startAnalysis() {
-		stop = false;
-		new Thread(new Analysis());
+		isAnalysing = true;
+		updateGraph();
 	}
 	
 	public void stopAnalysis() {
-		stop = true;
+		isAnalysing = false;
+		updateGraph();
 	}
-
-	private class Analysis implements Runnable {
-		@Override
-		public void run() {
-			
-			while(!stop) {
-				repaint();
-			}
-			
-		}
-	}	
+	
 	
 	@Override
 	public void paint(Graphics g) {
 		//Whose Graphics is that
-		System.out.println(g + "\n" + g.equals(this.getGraphics()) + "\n" + getGraphics());
-		try { Thread.sleep(1000); } catch (Exception e) {}
+		drawScale(g);
+		if (isAnalysing) {
+			drawGraph(g);
+		}
 	}
 	
-	public boolean isAnalysing() {
-		return !stop;
+	private void drawScale(Graphics g) {
+		final int dotsPerLetter = 6;
+		int arrowOffset = 3;
+		g.setColor(scaleColor);
+		int offset = this.getHeight() - scaleTextOffsetY;
+		g.drawLine(scaleTextOffsetX, arrowOffset, scaleTextOffsetX, offset); //y-Line
+		g.drawLine(scaleTextOffsetX, offset, this.getWidth() - arrowOffset, offset); //x-Line
+		g.drawLine(scaleTextOffsetX, arrowOffset, scaleTextOffsetX - arrowOffset, arrowOffset + 3); //Arrow Lines for y-Line
+		g.drawLine(scaleTextOffsetX, arrowOffset, scaleTextOffsetX + arrowOffset, arrowOffset + 3);
+		offset = this.getHeight() - scaleTextOffsetY;
+		g.drawLine(getWidth() - arrowOffset, offset, getWidth() - 3 - arrowOffset, offset - 3); //Arrow Lines for x-Line
+		g.drawLine(getWidth() - arrowOffset, offset, getWidth() - 3 - arrowOffset, offset + 3);
+		g.setFont(new Font(null, Font.BOLD, 12));
+		g.drawString("Hz",  this.getWidth() - 3 - 14, getHeight() - 2);
+		g.drawString("Amp",  scaleTextOffsetX + 7,  15);
+		g.setFont(new Font(null, Font.PLAIN, 11));
+		//draw Frequencies on X-Axis
+		int offsetPerText = dotsPerLetter * String.valueOf(maxFreq).length() + 8;
+		int axisLen = this.getWidth() - scaleTextOffsetX - arrowOffset;
+		int numSegments = axisLen / offsetPerText;
+		for(int i = 1; i < numSegments; ++i) {
+			String freq = String.valueOf(i * (maxFreq - minFreq) / numSegments + minFreq);
+			g.drawString(freq, scaleTextOffsetX + offsetPerText * i - freq.length() * dotsPerLetter / 2, this.getHeight() - 2);
+			g.drawLine(scaleTextOffsetX + offsetPerText * i, this.getHeight() - scaleTextOffsetY + 3, scaleTextOffsetX + offsetPerText * i, this.getHeight() - scaleTextOffsetY - 3);
+		}
+		//draw Amplitudes on Y-Axis
+		offsetPerText = 20;
+		axisLen = this.getHeight() - scaleTextOffsetY - arrowOffset;
+		numSegments = axisLen / offsetPerText;
+		for(int i = 1; i < numSegments; ++i) {
+			String amp = String.valueOf(i *  maxAmp / numSegments);
+			int yPosition = this.getHeight() - scaleTextOffsetY - i * offsetPerText;
+			g.drawString(amp, 2, yPosition + 5);
+			g.drawLine(scaleTextOffsetX - 3, yPosition , scaleTextOffsetX + 3, yPosition );
+		}
+	}
+	
+	private void drawGraph(Graphics g) {
+		int axisLen = this.getWidth() - scaleTextOffsetX - 3;
+		g.setColor(graphColor);
+		if (format.getSampleSizeInBits() == 16) {
+			//Don't display value 0(The DC Offset)
+			for(int i = 1; i < sSignal.length; ++i) {
+				g.fillRect(i * sSignal.length / axisLen, scaleTextOffsetY + sSignal[i], axisLen / sSignal.length, sSignal[i]);
+			}
+		}
 	}
 
+	public boolean isAnalysing() {
+		return isAnalysing;
+	}
+
+	public AudioFormat getFormat() {
+		return format;
+	}
+
+	public void setFormat(AudioFormat format) {
+		this.format = format;
+	}
+
+	public void setDarkColorTheme() {
+		this.bgColor = new Color(0, 0, 0);
+		this.scaleColor = new Color(240, 240, 240);
+		this.graphColor = new Color(0, 255, 0);
+		this.setBackground(bgColor);
+	}
+	
+	public void setBrightColorTheme() {
+		this.bgColor = new Color(240, 240, 240);
+		this.scaleColor = new Color(0, 0, 0);
+		this.graphColor = new Color(50, 255, 50);
+		this.setBackground(bgColor);
+	}
 	
 }
--- src/GUI/MicAnalysisWindow.java
+++ src/GUI/MicAnalysisWindow.java
@@ -7,6 +7,9 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.*;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 
 @SuppressWarnings("serial")
@@ -14,17 +17,30 @@ public class MicAnalysisWindow extends JFrame implements ActionListener {
 	
 	private AudioFrequencyDisplay  signalDisplay;
 	private JButton toggleButton;
+	private JMenuItem mDark, mBright;
 	
 	public MicAnalysisWindow() {
 		super("Mikrophon Audioanalyse");
 		this.add(new JLabel("Zeichenfläche:"), BorderLayout.NORTH);
-		signalDisplay = new AudioFrequencyDisplay(0, 20000, 300, 300);
+		signalDisplay = new AudioFrequencyDisplay(0, 20000, 600, 600);
 		
 		this.add(signalDisplay, BorderLayout.CENTER);
 		
 		toggleButton = new JButton("Start Analysis");
+		toggleButton.setSize(150, 50);
 		toggleButton.addActionListener(this);
 		this.add(toggleButton, BorderLayout.EAST);
+		
+		JMenuBar mb = new JMenuBar();
+		this.setJMenuBar(mb);
+		JMenu m = new JMenu("Color theme");
+		mb.add(m);
+		mDark = new JMenuItem("Dark theme");
+		mBright = new JMenuItem("Bright theme");
+		m.add(mDark);
+		m.add(mBright);
+		mDark.addActionListener(this);
+		mBright.addActionListener(this);
 		
 		this.addWindowListener(new WindowAdapter() {
 			@Override
@@ -32,12 +48,8 @@ public class MicAnalysisWindow extends JFrame implements ActionListener {
 				dispose();
 			}
 		});
-		
 		this.pack();
 		this.setVisible(true);
-		this.getGraphics().setColor(new Color(255, 0, 0));
-		this.getGraphics().drawRect(30,  30,  50,  100);
-		update(this.getGraphics());
 		signalDisplay.startAnalysis();
 		
 	}
@@ -53,6 +65,10 @@ public class MicAnalysisWindow extends JFrame implements ActionListener {
 				signalDisplay.startAnalysis();
 				toggleButton.setText("Stop Analysis");
 			}
+		} else if (e.getSource().equals(mDark)) {
+			signalDisplay.setDarkColorTheme();
+		} else if (e.getSource().equals(mBright)) {
+			signalDisplay.setBrightColorTheme();
 		}
 	}
 	
